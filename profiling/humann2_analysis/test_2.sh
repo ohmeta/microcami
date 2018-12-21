c2809d5b917d6f45c029174ed90155761f3c78d5
alienzj <alienchuj@gmail.com>
2018-12-21 10:03:11 +0800

    add profiling

---
+++ profiling/humann2_analysis/test_2.sh
@@ -0,0 +1,1 @@
+humann2 --input /ldfssz1/ST_META/share/User/zhujie/toolkit/humann2/examples/demo.fastq --output humann2_output_2
---
+++ profiling/metaphlan2_analysis/test.sh
@@ -0,0 +1,1 @@
+metaphlan2.py data/SRS014476-Supragingival_plaque.fasta.gz  --input_type fasta > profile/SRS014476-Supragingival_plaque_profile.txt
